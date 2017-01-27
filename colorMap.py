f6a0567bd787348ce39846c346556c1e5b22251e
JackLindgren <jtlindgren88@gmail.com>
2017-01-26 21:56:31 -0800

    first commit

---
+++ colorMap.py
@@ -0,0 +1,127 @@
+from colour import Color
+import csv
+import xml.etree.ElementTree as ET
+
+def getColors(numberNeeded):
+	"""
+	takes an int and returns a list of hex color values
+	"""
+	if(numberNeeded % 2 != 0):
+		numberNeeded += 1
+
+	myYellow = Color("#F4EB37")
+	myOrange = Color("#FFA500")
+	myRed    = Color("#8B0000")
+	colors1 = list(myYellow.range_to(myOrange, (numberNeeded / 2)))
+	colors2 = list(myOrange.range_to(myRed, (numberNeeded / 2)))
+
+	allColors = []
+	for color in colors1:
+		allColors.append(color.hex)
+	for color in colors2[1:]:
+		allColors.append(color.hex)
+	return allColors
+
+def matchValuesToColors(limits, colors):
+	"""
+	takes a list of limits and a list of colors
+	returns a dictionary of color matches {Value: Color}
+	"""
+
+	colorMatches = {}
+	i = 0
+	while i < len(limits):
+		colorMatches[limits[i]] = colors[i]
+		i += 1
+	return colorMatches
+
+def stateValuesDict():
+	# create a dictionary of {State: Value}
+	stateValueDict = {}
+
+	ourCSV = raw_input("Enter the file to open: ")
+
+	myFile = open(ourCSV, 'rb')
+	rows = csv.reader(myFile, delimiter=',')
+	for row in rows:
+		stateValueDict[row[0]] = int(row[1])
+
+	myFile.close()
+
+	return stateValueDict
+
+def getAllValues():
+	# this will hold the set of all of our limits
+	allValues = []
+
+	# open the CSV and read out all of the speed limits
+	myFile = open('SpeedByState.csv', 'rb')
+	rows = csv.reader(myFile, delimiter=',')
+	for row in rows:
+		allValues.append(int(row[1]))
+
+	myFile.close()
+
+	return allValues
+
+def getAllStates():
+	allStates = []
+	myFile = open('SpeedByState.csv', 'rb')
+	rows = csv.reader(myFile, delimiter=',')
+	for row in rows:
+		allStates.append(row[0])
+	myFile.close()
+	return allStates
+
+def colorStates(colorDict, stateValueDict):
+	"""
+	takes the {Value: Color} and {State: Value} dicts
+	and colors in the map
+	"""
+	# open and parse the map XML
+	tree = ET.parse('USMap.svg')
+	root = tree.getroot()
+
+	# iterate through the states
+	for child in root:
+		if child.tag == "{http://www.w3.org/2000/svg}g":
+			for state in child:
+				try:
+					# find the state's value
+					stateValue = stateValueDict[state.attrib["id"]]
+					# find the corresponding color
+					stateColor = colorDict[stateValue]
+					# write that color to the shape's space on the map
+					state.attrib["fill"] = stateColor
+				except KeyError:
+					continue
+
+	# write the new XML to the output file
+	output = ET.tostring(root)
+	f = open('newMap.svg', 'w')
+	f.write(output)
+	f.close()
+
+# get a list of all states
+allStates = getAllStates()
+
+# get a list of all values associated with the states
+allValues = getAllValues()
+
+# figure out how many colors we need
+colorsNeeded = max(allValues) - min(allValues)
+
+# get the color palette
+ourColors = getColors(colorsNeeded)
+
+uniqLimits = list(set(allValues))
+uniqLimits.sort()
+
+# get the color dictionary 
+colorDict = matchValuesToColors(uniqLimits, ourColors)
+
+# get the {State: Speed} dictionary
+stateValueDict = stateValuesDict()
+
+colorStates(colorDict, stateValueDict)
+
