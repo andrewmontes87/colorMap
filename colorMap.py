c232a47e62d391f65708217df1d546646eee289f
JackLindgren <jtlindgren88@gmail.com>
2017-01-28 16:28:51 -0800

    handle unnabreviated state names in the CSV

--- colorMap.py
+++ colorMap.py
@@ -4,6 +4,9 @@ import xml.etree.ElementTree as ET
 import sys
 import re
 
+statesByAbbrev = {"AL": "Alabama", "AK": "Alaska", "AZ": "Arizona", "AR": "Arkansas", "CA": "California", "CO": "Colorado", "CT": "Connecticut", "DE": "Delaware", "DC": "District of Columbia", "FL": "Florida", "GA": "Georgia", "HI": "Hawaii", "ID": "Idaho", "IL": "Illinois", "IN": "Indiana", "IA": "Iowa", "KS": "Kansas", "KY": "Kentucky", "LA": "Louisiana", "ME": "Maine", "MD": "Maryland", "MA": "Massachusetts", "MI": "Michigan", "MN": "Minnesota", "MS": "Mississippi", "MO": "Missouri", "MT": "Montana", "NE": "Nebraska", "NV": "Nevada", "NH": "New Hampshire", "NJ": "New Jersey", "NM": "New Mexico", "NY": "New York", "NC": "North Carolina", "ND": "North Dakota", "OH": "Ohio", "OK": "Oklahoma", "OR": "Oregon", "PA": "Pennsylvania", "RI": "Rhode Island", "SC": "South Carolina", "SD": "South Dakota", "TN": "Tennessee", "TX": "Texas", "UT": "Utah", "VT": "Vermont", "VA": "Virginia", "WA": "Washington", "WV": "West Virginia", "WI": "Wisconsin", "WY": "Wyoming"}
+
+
 def getColors(numberNeeded, userColors):
 	"""
 	takes the number needed and returns a list of hex color values
@@ -58,16 +61,20 @@ def matchValuesToColors(values, colors):
 		i += 1
 	return colorMatches
 
-def stateValuesDict(inputCSV):
+def stateValuesDict(inputCSV, needAbbreviate):
 	# create a dictionary of {State: Value}
 	stateValueDict = {}
 
-	# ourCSV = raw_input("Enter the file to open: ")
+	# dictionary of all state {Name: Abbreviation}
+	statesByName = {"Alabama": "AL", "Alaska": "AK", "Arizona": "AZ", "Arkansas": "AR", "California": "CA", "Colorado": "CO", "Connecticut": "CT", "Delaware": "DE", "District of Columbia": "DC", "Florida": "FL", "Georgia": "GA", "Hawaii": "HI", "Idaho": "ID", "Illinois": "IL", "Indiana": "IN", "Iowa": "IA", "Kansas": "KS", "Kentucky": "KY", "Louisiana": "LA", "Maine": "ME", "Maryland": "MD", "Massachusetts": "MA", "Michigan": "MI", "Minnesota": "MN", "Mississippi": "MS", "Missouri": "MO", "Montana": "MT", "Nebraska": "NE", "Nevada": "NV", "New Hampshire": "NH", "New Jersey": "NJ", "New Mexico": "NM", "New York": "NY", "North Carolina": "NC", "North Dakota": "ND", "Ohio": "OH", "Oklahoma": "OK", "Oregon": "OR", "Pennsylvania": "PA", "Rhode Island": "RI", "South Carolina": "SC", "South Dakota": "SD", "Tennessee": "TN", "Texas": "TX", "Utah": "UT", "Vermont": "VT", "Virginia": "VA", "Washington": "WA", "West Virginia": "WV", "Wisconsin": "WI", "Wyoming": "WY"}
 
 	myFile = open(inputCSV, 'rb')
 	rows = csv.reader(myFile, delimiter=',')
 	for row in rows:
-		stateValueDict[row[0]] = int(row[1])
+		if needAbbreviate == False:
+			stateValueDict[row[0]] = int(row[1])
+		elif needAbbreviate == True:
+			stateValueDict[statesByName[row[0]]] = int(row[1])
 
 	myFile.close()
 
@@ -164,6 +171,9 @@ inputCSV = getInputFile(sys.argv)
 
 # get a list of all states
 allStates = getAllStates(inputCSV)
+needAbbreviate = False
+if len(allStates[0]) > 2:
+	needAbbreviate = True
 
 # get a list of all values associated with the states
 allValues = getAllValues(inputCSV)
@@ -196,7 +206,7 @@ else:
 	colorDict = matchValuesToColors(uniqValues, ourColors)
 
 # get the {State: Speed} dictionary
-stateValueDict = stateValuesDict(inputCSV)
+stateValueDict = stateValuesDict(inputCSV, needAbbreviate)
 
 outputCSV = getOutputFile(sys.argv)
 
