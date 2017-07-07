db10e16c71f8f3b72fe1feb3b61600b97e87544c
andrewmontes87 <andrewmontes87@gmail.com>
2017-07-06 18:28:04 -0700

    package refactoring

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+
+
+
+
+v1.1.0
+- virtualenv venv
+- runtime
+- gitignore
+- install packages with pip
+- - flask
+- - ipython
+- - gunicorn
+- - colour
+- freeze requirements
+- restructure directories
+
+
+v1.0.0
+- release
+
---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='colorMap',
+    version='1.1.0',
+    packages=['colorMap'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'colour',
+    ],    
+    description='Create color maps using CSVs',
+    author='Jack Lindgren + Andrew Montes',
+    long_description=open('README.md').read(),
+)
